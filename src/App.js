fbaccb27631765dafc884503af5b3da0952014c0
innonka <innon@yandex.ru>
2023-02-16 16:24:44 +0200

    add auth0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { user, isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+
+  useEffect(() => {
+    console.log("isAuthenticated: " + isAuthenticated);
+    if (isAuthenticated == false) {
+      //loginWithRedirect();
+    }
+  }, []);
+
   return <div className="bg-blue-600">hello</div>;
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="{yourDomain}" // TODO: get domain
+      clientId="{yourClientId}" // TODO: get client id
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
